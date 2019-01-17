fcf44bb622c9cebdfcc329deb3800e2d48ef0038
digitalfinesse <dizverst@gmail.com>
2019-01-18 01:29:08 +0300

    add frontend techs - vue, etc. and add django-cors-headers

--- backend/simplenote/settings.py
+++ backend/simplenote/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'notes',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -50,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'simplenote.urls'
@@ -121,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = True
